7a27dfa26a39545e6eca9c27e26e8d416e4a1602
patrictobola <tobola92@gmail.com>
2023-06-12 15:08:16 +0200

    milestone 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 console.log('JS OK')
+
+const staff = {
+    name: ["Wayne Barnett", "Angela Caroll", "Walter Gordon", "Angela Lopez", "Scott Estrada", "Barbara Ramos"],
+    role: ["Founder & CEO", "Chief Editor", "Office Manager", "Social Media Manager", "Developer", "Graphic Designer"],
+    profilePic: ["wayne-barnett-founder-ceo.jpg", "angela-caroll-chief-editor.jpg", "walter-gordon-office-manager.jpg", "angela-lopez-social-media-manager.jpg", "scott-estrada-developer.jpg", "barbara-ramos-graphic-designer.jpg"]
+}
+
